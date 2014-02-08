b8f25bef32030226609bba0879335e6e7a02742a
Charles H <chathaway@logrit.com>
2014-02-07 21:24:24 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JojoPoems::Application.config.secret_key_base = '1012bf12bc33c2fccc8809fb187b852b7aaf0617740d9b0eef23c76e255ab68f1711d16d29cfc1e2c83821ca1137c6d5b960e601c9bceba039ed90196e090a40'
